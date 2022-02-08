4a3f8912ad5c44777e7e1e251341a76cb6bff8ca
senaayavuz0@gmail.com <senaayavuz0@gmail.com>
2022-02-08 16:50:18 +0300

    2.soru cozuldu

--- NortwindDb/JoinSoruları.sql
+++ NortwindDb/JoinSoruları.sql
@@ -8,3 +8,9 @@ Group by e.FirstName, e.LastName, year(o.OrderDate)
 Order by e.FirstName, yil
 
 --2: Müsterilerimin 1997 1.çeyrekte verdiği siparis sayısı nedir?
+Select c.CompanyName, count(o.OrderID) Adet
+from Customers c
+inner join Orders o on o.CustomerID=c.CustomerID
+where year(o.OrderDate)=1997 
+and MONTH(o.OrderDate) between 1 and 3
+Group By c.CompanyName
