727998de8763af5c72e4c279da91c832ff907ddb
senaayavuz0@gmail.com <senaayavuz0@gmail.com>
2022-02-08 16:31:10 +0300

    2. soru cozuldu

---
+++ NortwindDb/JoinSoruları.sql
@@ -0,0 +1,10 @@
+﻿--SORULAR 
+
+--1: calisanların yıllara göre satis dagılımı nedir ? FirstName, LastName, Count(*)
+Select e.FirstName, e.LastName, year(o.OrderDate) yil, Count(o.OrderID) 
+from Orders o 
+inner join Employees e on o.EmployeeID=e.EmployeeID
+Group by e.FirstName, e.LastName, year(o.OrderDate)
+Order by e.FirstName, yil
+
+--2: Müsterilerimin 1997 1.çeyrekte verdiği siparis sayısı nedir?
